e25f82a63291e60cc86bebe3e3a3918ea36842ec
OPIXX.my.id <fattanmybee@gmail.com>
2026-02-02 23:31:01 +0800

    Add utility functions for sleep and randomString
    Added utility functions for sleep and random string generation.

--- javascrip/tindex.js
+++ javascrip/tindex.js
@@ -1,1 +1,20 @@
+/**
+ * Simple utility functions
+ * Used for project tooling
+ */
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function randomString(length = 8) {
+  const chars = "abcdefghijklmnopqrstuvwxyz0123456789";
+  let result = "";
+  for (let i = 0; i < length; i++) {
+    result += chars[Math.floor(Math.random() * chars.length)];
+  }
+  return result;
+}
+
+module.exports = { sleep, randomString };
 
